6e5a66801505e5ddf4f1c4fd316e8ff96c56bb50
Leonard Theodorus <aoesotfs@gmail.com>
2023-05-19 09:30:01 +0700

    Initial Commit

---
+++ WaterWise/WaterWiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterWiseApp.swift
+//  WaterWise
+//
+//  Created by Leonard Theodorus on 19/05/23.
+//
+
+import SwiftUI
+
+@main
+struct WaterWiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
